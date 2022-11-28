50dc0f302f9d57701e063df8456d1325d681997d
Alina Malyutina <linamalutina1@gmail.com>
2022-11-28 18:28:37 +0300

    add task 64 - recursion 1 to N

---
+++ task64/Program.cs
@@ -0,0 +1,20 @@
+﻿void PrintNumbersBetween1toN(int N, int count)
+{
+    if (count > N)
+    {
+        return;
+    }
+    else
+    {
+        PrintNumbersBetween1toN(N, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+Console.WriteLine("This program assepts number and show all numbers between 1 to N");
+Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+PrintNumbersBetween1toN(N, count);
+
+
