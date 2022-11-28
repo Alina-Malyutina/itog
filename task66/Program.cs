500a3fef56b788d810ef600bf0a7a5f54ba7a807
Alina Malyutina <linamalutina1@gmail.com>
2022-11-28 18:39:42 +0300

    add task 66 - count sum between N and M entered by user

---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿int SumBetweenNAndM(int N, int M)
+{
+    int sum = 0;
+    for (int i = N; i <= M; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("This program assepts M and N and show sum of elements between N and M");
+Console.Write("Enter M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int summa = SumBetweenNAndM(M, N);
+Console.WriteLine(summa);
+
